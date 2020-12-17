fe8fdea1fb7a800dd947da72e939cfb1e07107ab
Hadrien Croubois <hc@iex.ec>
2020-12-17 10:36:20 +0100

    add support for default providerOrSigner + JSONRpcSigner

--- src/utils/executeTxCli.js
+++ src/utils/executeTxCli.js
@@ -2,14 +2,31 @@
 
 const prompts                                 = require('prompts');
 const isUrl                                   = require('is-url');
+const { Provider                            } = require('@ethersproject/abstract-provider');
+const { Signer                              } = require('@ethersproject/abstract-signer');
 const { isAddress                           } = require('@ethersproject/address');
 const { isValidName                         } = require('@ethersproject/hash');
 const { getDefaultProvider, JsonRpcProvider } = require('@ethersproject/providers');
 const { Wallet                              } = require('@ethersproject/wallet');
 const { LedgerSigner                        } = require('@ethersproject/hardware-wallets');
 
-async function executeTxCli(txRequest = {})
+
+async function executeTxCli(txRequest = {}, providerOrSigner = null)
 {
+	prompts.override({
+		to: txRequest.to,
+		provider:
+			providerOrSigner instanceof Provider
+			? providerOrSigner
+			: providerOrSigner instanceof Signer
+			? providerOrSigner.provider
+			: undefined,
+		signer:
+			providerOrSigner instanceof Signer
+			? providerOrSigner
+			: undefined,
+	});
+
 	const { execute, provider, to, signer } = await prompts([{
 		type: 'select',
 		name: 'execute',
@@ -31,7 +48,7 @@ async function executeTxCli(txRequest = {})
 			{ value: 'http://localhost:8545'        },
 			{ title: 'custom endpoint', value: null },
 		],
-		format: chain => chain && getDefaultProvider(chain),
+		format: (chain) => typeof chain == 'string' ? getDefaultProvider(chain) : chain
 	},{
 		type: (_, { execute, provider }) => execute && !provider && 'text',
 		name: 'provider',
@@ -40,32 +57,42 @@ async function executeTxCli(txRequest = {})
 		validate: isUrl,
 		format: endpoint => new JsonRpcProvider(endpoint),
 	},{
-		type: (_, { execute }) => execute && !txRequest.to && 'text',
+		type: (_, { execute }) => execute && 'text',
 		name: 'to',
-		message: 'Address of the instance',
+		message: 'Address of the target',
 		validate: address => isAddress(address) || isValidName(address),
 	},{
 		type: (_, { execute }) => execute && 'select',
-		name: 'signertype',
+		name: 'signer',
 		message: 'Select your wallet type',
-		choices: [
-			{ title: 'Private key',            value: 'wallet' },
-			{ title: 'Ledger hardware wallet', value: 'ledger' },
-		],
+		choices: (_, { provider}) => [{
+			title: 'Private key',
+			value: 'wallet'
+		},{
+			title: 'JsonRpc signer',
+			value: provider instanceof JsonRpcProvider && provider.getSigner(),
+			disabled: !(provider instanceof JsonRpcProvider),
+		},{
+			title: 'Ledger hardware wallet',
+			value: 'ledger'
+		}],
 	},{
-		type: (_, { signertype }) => signertype == 'wallet' && 'text',
+		type: (_, { signer }) => signer == 'wallet' && 'text',
 		name: 'signer',
 		message: 'Private key of the wallet',
 		initial: process.env.MNEMONIC,
 		validate: pk => /^0x[0-9a-z]{64}$/.exec(pk),
 		format: (pk, { provider }) => new Wallet(pk, provider),
 	},{
-		type: (_, { signertype }) => signertype == 'ledger' && 'text',
+		type: (_, { signer }) => signer == 'ledger' && 'text',
 		name: 'signer',
 		message: 'Path',
 		initial: 'm/44\'/60\'/0\'/0/0',
 		format: (path, { provider }) => new LedgerSigner(provider, 'hid', path),
 	}]);
+
+	if (execute == undefined) throw 'Aborted';
+	if (execute && signer == undefined) throw 'Aborted';
 
 	if (execute)
 	{
